e26aa5affe4d76cdcf4fa9fef7984a1800950e73
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-19 12:01:06 +0200

    Data from site

--- pages/reviews.js
+++ pages/reviews.js
@@ -1,6 +1,20 @@
 import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
 
 export default function Reviews() {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/comments',
+      );
+      const data = await response.json();
+      setReviews(data);
+    };
+    getData();
+  }, []);
+  console.log(reviews);
+
   return (
     <>
       <Head>
@@ -9,6 +23,18 @@ export default function Reviews() {
       </Head>
       <div>
         <h1>Reviews</h1>
+        <div>
+          {!!reviews.length &&
+            reviews.slice(0, 20).map((res) => {
+              return (
+                <div key={res.id} style={{ margin: '15px' }}>
+                  {res.id}
+                  {'. '}
+                  {`${res.body.slice(0, 90)}...`}
+                </div>
+              );
+            })}
+        </div>
       </div>
     </>
   );
